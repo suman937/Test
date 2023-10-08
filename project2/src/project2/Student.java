cf154a570ec2bdb27ae0641c8d1ab1eb8afbf9d2
suman937 <suman937@yahoo.com>
2023-10-08 12:52:03 -0400

    third

--- project2/src/project2/Student.java
+++ project2/src/project2/Student.java
@@ -3,7 +3,7 @@ package project2;
 public class Student {
 
 	public static void main(String[] args) {
-		System.out.println("This is main class.");
+		System.out.println("This is the main class.");
 
 	}
 
